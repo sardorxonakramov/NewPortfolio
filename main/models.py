2d677b3de3190e23573f9a524d0d6f3433326655
sardorxonakramov <sardorxonakramov01@gmail.com>
2026-02-24 19:49:54 +0500

    Message Model Created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+
+class MessageForMe(models.Model):
+    fullname = models.CharField(max_length=40, verbose_name="Full Name")
+    phone = models.CharField(
+        max_length=13,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r"^\+998\d{9}$",
+                message="Telefon raqam formati: +998901234567 bo'lishi kerak.",
+            )
+        ],
+        help_text="Masalan: +998901234567",
+    )
+    subject = models.CharField(
+        max_length=50,
+        verbose_name="Mavzu",
+        null=True,
+        blank=True,
+    )
+
+    message = models.TextField(verbose_name="Xabar matni: ")
